38d3d7efca564d244fd57f0999d6d28570502424
Dana Popa <popa.dana100@gmail.com>
2024-04-17 10:21:02 +0300

    initial commit with main page skeleton

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Measurement Conversion</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="main-content">
+		<h1>Measurement Conversion</h1>
+		<ul id="measurement-types">
+		    <li><a href="temperature.php">Temperature</a></li>
+			<li><a href="speed.php">Speed</a></li>
+			<li><a href="mass.php">Mass</a></li>
+		</ul>
+	</div>
+</body>
+</html>
