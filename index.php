c2a3cf504e59373279127f305cd91b584b94d9cd
Dana Popa <popa.dana100@gmail.com>
2024-04-17 12:16:54 +0300

    main page UI style

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Measurement Conversion</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8">
+    <title>Measurement Conversion</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-	<div id="main-content">
-		<h1>Measurement Conversion</h1>
-		<ul id="measurement-types">
-		    <li><a href="temperature.php">Temperature</a></li>
-			<li><a href="speed.php">Speed</a></li>
-			<li><a href="mass.php">Mass</a></li>
-		</ul>
-	</div>
-<div id="footer">
-    <p>&copy; <?php echo date("Y"); ?> Dana Popa</p>
-</div>
+    <div class="container">
+        <header>
+            <h1>Measurement Conversion</h1>
+        </header>
+        <div id="main-content" class="convertor-container">
+            <ul id="measurement-types">
+                <li class="measurement-button"><a href="temperature.php">Temperature</a></li>
+                <li class="measurement-button"><a href="speed.php">Speed</a></li>
+                <li class="measurement-button"><a href="mass.php">Mass</a></li>
+            </ul>
+        </div>
+        <footer>
+            <p>&copy; <?php echo date("Y"); ?> Dana Popa</p>
+        </footer>
+    </div>
 </body>
 </html>
